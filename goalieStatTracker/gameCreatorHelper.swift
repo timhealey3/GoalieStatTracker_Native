30645762d4109030603b200592bd5b40e04c72eb
Timothy Healey <timhealey34@gmail.com>
2024-10-05 23:09:17 -0400

    add some vars

--- goalieStatTracker/gameCreatorHelper.swift
+++ goalieStatTracker/gameCreatorHelper.swift
@@ -10,5 +10,15 @@ import Foundation
 class gameCreatorHelper : ObservableObject {
     // init var
     @Published var opponent: String = ""
+    @Published var minutes: Int = 0
+    @Published var startingGoalie: Bool = false;
+
+    // func that finds opponents
+    func findTeams() -> [Team] {
+    
+    }
+
+    // func that filters opponents
+    func filterTeams() -> [Team]
 }
 
